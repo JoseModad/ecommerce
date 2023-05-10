35aefa8c17846b44c2bace04d870e1b2c6e7de6b
turko2373 <turko1073@gmail.com>
2023-05-10 01:08:28 -0300

    modifique error de importacion de la clase setting

--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from core.config import settings
+from app.core.config import setting
 
 
-SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+SQLALCHEMY_DATABASE_URL = setting.DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
