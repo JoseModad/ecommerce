b5ed94a6fb67a9624cdec6581165f2ccd3b5e138
turko2373 <turko1073@gmail.com>
2023-05-10 17:32:18 -0300

    manejo de registro incorrecto

--- app/routers/productos.py
+++ app/routers/productos.py
@@ -1,5 +1,5 @@
 from fastapi import Request, APIRouter, File,  UploadFile, Form, Depends
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from app.repository.manejo import guardar_producto, consultastockproducto
 from app.db.database import get_db
@@ -8,32 +8,42 @@ from app.db import modelos
 from app.schemas import RegistroForm
 
 
+
 router = APIRouter(include_in_schema = False)
 
 templates = Jinja2Templates(directory="app/templates")
 
-@router.get("/usuarios")
-def ruta(db: Session = Depends(get_db)):
-    usuarios = db.query(modelos.User).all()
-    print(usuarios)
-    return usuarios
 
+# funcion usada para crear las tablas de la base de datos. se usara mas adelante para el panel de administracion
+
+# @router.get("/usuarios")
+# def ruta(db: Session = Depends(get_db)):
+#     usuarios = db.query(modelos.User).all()
+#     print(usuarios)
+#     return usuarios
+
+
+# Funciones para el panel de Usuario
 
 @router.get("/", response_class=HTMLResponse)
-def  home(request: Request):
+def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
 
 @router.get("/nosotros")
 def nosotros(request: Request):
     return templates.TemplateResponse("nosotros.html", {"request": request})
 
+
 @router.get("/proveedores")
 def proveedores(request: Request):
     return templates.TemplateResponse("proveedores.html", {"request": request})
 
+
 @router.get("/ingresar")
 def cliente(request: Request):
     return templates.TemplateResponse("ingresar.html", {"request": request})
+
 
 @router.post("/login")
 async def login(request: Request, db: Session = Depends(get_db)):    
@@ -49,31 +59,41 @@ async def login(request: Request, db: Session = Depends(get_db)):
 
 @router.get("/registrarse")
 def registrarse(request: Request):
-    return templates.TemplateResponse("registrarse.html", {"request": request}) 
-
+    return templates.TemplateResponse("registrarse.html", {"request": request, "mensaje": None})
+    
 
 @router.post("/registro")
 async def registro(request: Request, db: Session = Depends(get_db)):
     form = RegistroForm(request)
-    await form.get_data()    
-    nuevo_usuario = modelos.User(
-        username = form.username,
-        password = form.password,
-        email = form.email,
-        nombre = form.nombre,
-        apellido = form.apellido,
-        telefono = form.telefono,
-        direccion = form.direccion,
-        ciudad = form.ciudad,
-        provincia = form.provincia,
-        pais = form.pais,
-        codigo_postal = form.codigo_postal
-    )        
-    db.add(nuevo_usuario)
-    db.commit()    
-    db.refresh(nuevo_usuario)
-    return templates.TemplateResponse("ingresar.html", {"request": request})
+    await form.get_data()
+    exists_username = db.query(modelos.User).filter(modelos.User.username == form.username).first()
+    exists_email = db.query(modelos.User).filter(modelos.User.email == form.email).first()
+    if exists_username or exists_email:
+        mensaje = "El usuario o email ya existen"
+        return templates.TemplateResponse("registrarse.html", {"request": request, "mensaje": mensaje})
 
+    else:         
+        nuevo_usuario = modelos.User(
+            username = form.username,
+            password = form.password,
+            email = form.email,
+            nombre = form.nombre,
+            apellido = form.apellido,
+            telefono = form.telefono,
+            direccion = form.direccion,
+            ciudad = form.ciudad,
+            provincia = form.provincia,
+            pais = form.pais,
+            codigo_postal = form.codigo_postal
+        )
+        db.add(nuevo_usuario)
+        db.commit()    
+        db.refresh(nuevo_usuario)
+        return templates.TemplateResponse("ingresar.html", {"request": request})
+
+
+
+# Funciones para el panel de administracion
 
 @router.get("/cargar-productos")
 def cargar_productos(request: Request):
@@ -97,5 +117,3 @@ async def consultas(request: Request, id: str = Form(...)):
         consulta = "Consulta para entregas"
     elif id == "botonPagos":
         consulta = "Consulta para pagos" 
-    
-
